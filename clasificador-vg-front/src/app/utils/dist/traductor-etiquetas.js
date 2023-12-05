db6791c0851fadde3cef6ab58f3717bc45bb8ab7
pintocristian <pintocristian@unicauca.edu.co>
2023-12-05 18:25:46 -0500

    actualizacion traductor de etiquetas

--- clasificador-vg-front/src/app/utils/dist/traductor-etiquetas.js
+++ clasificador-vg-front/src/app/utils/dist/traductor-etiquetas.js
@@ -8,6 +8,8 @@ var TraductorEtiquetas = /** @class */ (function () {
         this.traducirPacienteHospitalizado(dataframe);
         this.traducirCondicionFinal(dataframe);
         this.traducirNaturaleza(dataframe);
+        this.traducirActividad(dataframe);
+        this.traducirSustanciasVictima(dataframe);
     };
     TraductorEtiquetas.traducirPacienteHospitalizado = function (dataframe) {
         var _a;
@@ -79,6 +81,80 @@ var TraductorEtiquetas = /** @class */ (function () {
                     break;
                 default:
                     console.warn("Valor inesperado en la columna 'naturaleza': " + valorOriginal);
+            }
+        });
+    };
+    TraductorEtiquetas.traducirActividad = function (dataframe) {
+        var _a;
+        // Verificar si la columna 'actividad' existe en el dataframe
+        if (!((_a = dataframe[0]) === null || _a === void 0 ? void 0 : _a.hasOwnProperty("actividad"))) {
+            console.error('La columna "actividad" no existe en el dataframe.');
+            return;
+        }
+        /* Se mapean los valores de las columnas para que en lugar de mostrar
+          valores numéricos muestre etiquetas más descriptivas
+        */
+        dataframe.forEach(function (item) {
+            var valorOriginal = item["actividad"];
+            switch (valorOriginal) {
+                case "13":
+                    item["actividad"] = "Líderes cívicos(as)";
+                    break;
+                case "15":
+                    item["actividad"] = "Maestros(as)";
+                    break;
+                case "16":
+                    item["actividad"] = "Servidor público(a)";
+                    break;
+                case "17":
+                    item["actividad"] = "Fuerza pública";
+                    break;
+                case "24":
+                    item["actividad"] = "Estudiante";
+                    break;
+                case "26":
+                    item["actividad"] = "Otro";
+                    break;
+                case "28":
+                    item["actividad"] = "Trabajadora doméstica";
+                    break;
+                case "29":
+                    item["actividad"] = "Persona en situación de prostitución";
+                    break;
+                case "31":
+                    item["actividad"] = "Persona dedicada al cuidado del hogar";
+                    break;
+                case "33":
+                    item["actividad"] = "Ninguna";
+                    break;
+                case "8":
+                    item["actividad"] = "Reciclador(a)";
+                    break;
+                default:
+                    console.warn("Valor inesperado en la columna 'actividad': " + valorOriginal);
+            }
+        });
+    };
+    TraductorEtiquetas.traducirSustanciasVictima = function (dataframe) {
+        var _a;
+        // Verificar si la columna 'sustancias_victima' existe en el dataframe
+        if (!((_a = dataframe[0]) === null || _a === void 0 ? void 0 : _a.hasOwnProperty("sustancias_victima"))) {
+            console.error('La columna "sustancias_victima" no existe en el dataframe.');
+            return;
+        }
+        /* Se mapean los valores de las columnas para que en lugar de mostrar
+          valores numéricos muestre etiquetas más descriptivas
+        */
+        dataframe.forEach(function (item) {
+            var valorOriginal = item["sustancias_victima"];
+            if (valorOriginal === "1") {
+                item["sustancias_victima"] = "Si";
+            }
+            else if (valorOriginal === "2") {
+                item["sustancias_victima"] = "No";
+            }
+            else {
+                console.warn("Valor inesperado en la columna 'sustancias_victima': " + valorOriginal);
             }
         });
     };

0e74892d2d8dff2c6c922ecdfd8fa161989aced4
pintocristian <pintocristian@unicauca.edu.co>
2024-02-19 17:27:46 -0500

    ajuste label

--- clasificador-vg-front/src/app/service/service.ts
+++ clasificador-vg-front/src/app/service/service.ts
@@ -1,23 +1,20 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { RegistroDto } from 'app/dto/registro-dto';
-const back_end_url = "https://predictor-violencia-genero.onrender.com/"
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { RegistroDto } from "app/dto/registro-dto";
+const back_end_url = "https://predictor-violencia-genero.onrender.com/";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class Service {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient : HttpClient) { }
-
-  public obtenerDF() : Observable<any> {
-    return this.httpClient.get(back_end_url + 'obtener_df/');
+  public obtenerDF(): Observable<any> {
+    return this.httpClient.get(back_end_url + "obtener_df/");
   }
 
-  public predecir(registroDTO : RegistroDto) : Observable<any> {
-    return this.httpClient.post<any>(back_end_url + 'predecir/', registroDTO);
+  public predecir(registroDTO: RegistroDto): Observable<any> {
+    return this.httpClient.post<any>(back_end_url + "predecir/", registroDTO);
   }
-
- 
 }
